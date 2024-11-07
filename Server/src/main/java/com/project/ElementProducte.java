38faba83e33ed9ee0d11e7ceab63224381202d95
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-11-07 15:28:37 +0100

    generarXML

---
+++ Server/src/main/java/com/project/ElementProducte.java
@@ -0,0 +1,18 @@
+package com.server;
+
+public class ElementProducte {
+    String id;
+    String nom;
+    double preu;
+    String descripcio;
+    String imatge;
+
+    public ElementProducte(String id, String nom, double preu, String descripcio, String imatge) {
+        this.id = id;
+        this.nom = nom;
+        this.preu = preu;
+        this.descripcio = descripcio;
+        this.imatge = imatge;
+    }
+}
+
